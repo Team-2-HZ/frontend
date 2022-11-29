0c49f812d9a14b4a8cebf1a4bb3345f8c89a66cb
LifeAdmin-cmd <lars.kra2001@gmail.com>
2022-11-29 15:13:16 +0100

    added base nutrition values

---
+++ src/components/DailyNutritionCard.vue
@@ -0,0 +1,22 @@
+<script>
+  const dailyNutritionMale = {
+    KCAL: 6500,
+    CARBS: 300,
+    SUGAR: 65,
+    FAT: 95,
+    PROTEIN: 55
+  }
+  const dailyNutritionFemale = {
+    KCAL: 2000,
+    CARBS: 230,
+    SUGAR: 49,
+    FAT: 73,
+    PROTEIN: 45
+  }
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-title">ehre alla</div>
+  </div>
+</template>
