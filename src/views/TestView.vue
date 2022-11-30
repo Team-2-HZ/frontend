531caecc135c01ce4f3f2c2e0f1ffcc1eeabbae1
Lars <lars.kra2001@gmail.com>
2022-11-30 12:01:44 +0100

    added charts.js

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,14 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 import DailyNutritionVue from '../components/charts/DailyNutrition.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
     <DailyNutritionVue />
   </main>
 </template>
