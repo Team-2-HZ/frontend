c3da12ad66f3e26b597dfc51be00e8eae7b05a2e
Lars <lars.kra2001@gmail.com>
2022-11-30 10:14:17 +0100

    added tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,8 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </main>
 </template>
