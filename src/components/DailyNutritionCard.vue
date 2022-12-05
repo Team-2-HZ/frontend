53f41058b2012ff0a928350af7bc380d31651f33
LifeAdmin-cmd <lars.kra2001@gmail.com>
2022-12-05 15:04:43 +0100

    added pug

--- src/components/DailyNutritionCard.vue
+++ src/components/DailyNutritionCard.vue
@@ -2,19 +2,13 @@
 import DailyNutrition from '../components/charts/DailyNutrition.vue';
 </script>
 
-<template>
-  <div class="card">
-    <div class="card-body">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm">
-            <DailyNutrition />
-          </div>
-          <div class="col-sm">
-            <DailyNutrition />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+.card
+  .card-body
+    .container
+      .row
+        .col-sm
+          DailyNutrition
+        .col-sm
+          DailyNutrition
 </template>
