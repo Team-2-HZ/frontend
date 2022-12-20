55d62723fa61b56842290bcd7cff3a0faa431b3f
TJM-HZ <mein0009@hz.nl>
2022-12-20 11:50:05 +0100

    Added dynamic colors to the progress bar.

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="progress">
-        <div class="progress-bar" 
+        <div class="progress-bar"
+        :class="color()"
         role="progressbar" 
         :style="{ 'width': ariaValue + '%'}" 
         :aria-valuenow="ariaValue"
@@ -25,10 +26,27 @@ export default {
             default: 100 // TODO: Prevent this from being set to 0. Please never set this to 0.
         },
     },
+    methods: {
+        color: function() {
+            // TODO: There's probably a better way of doing this.
+            let lowerLimit = this.targetValue * 0.95;
+            let upperLimit = this.targetValue * 1.05;
+            if (this.currentValue < lowerLimit) {
+                console.log('NOT ENOUGH')
+                return "bg-warning"
+            } else if (this.currentValue >= lowerLimit && this.currentValue <= upperLimit) {
+                console.log('JUST RIGHT')
+                return "bg-success"
+            } else {
+                console.log('TOO MUCH')
+                return "bg-danger"
+            }
+        }
+    },
     computed: {
         ariaValue() {
             return Math.round(this.currentValue / this.targetValue * 100);
-        }
+        },        
     }
 }
 </script>
