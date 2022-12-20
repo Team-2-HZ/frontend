ab91c67358e811c070b2c5ea5c8ff16b886670da
TJM-HZ <mein0009@hz.nl>
2022-12-20 10:37:58 +0100

    Created a basic progress bar component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="progress">
+        <div class="progress-bar" 
+        role="progressbar" 
+        :style="{ 'width': ariaValue + '%'}" 
+        :aria-valuenow="ariaValue"
+        aria-valuemin="0" 
+        aria-valuemax="100">
+        {{ currentValue }} / {{ targetValue }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProgressBar',
+    props: {
+        currentValue: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        targetValue: {
+            type: Number,
+            required: true,
+            default: 100 // TODO: Prevent this from being set to 0. Please never set this to 0.
+        },
+    },
+    computed: {
+        ariaValue() {
+            return Math.round(this.currentValue / this.targetValue * 100);
+        }
+    }
+}
+</script>
